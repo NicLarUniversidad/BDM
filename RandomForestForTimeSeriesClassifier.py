22461f62a00ebdd4a29cf1ccf065bfd1e410c0d8
NicLarUniversidad <71468011+NicLarUniversidad@users.noreply.github.com>
2024-12-03 18:43:41 -0300

    Implementación de RFTS

--- RandomForestForTimeSeriesClassifier.py
+++ RandomForestForTimeSeriesClassifier.py
@@ -16,6 +16,58 @@ from sklearn.utils.validation import _check_sample_weight
 MAX_INT = np.iinfo(np.int32).max
 
 class RandomForestForTimeSeriesClassifier(RandomForestClassifier):
+
+    # Llamo al constructor de RandomForestClassifier
+    # Necesario porque quiero una variable con el tamaño de bloques
+    # block_size
+    def __init__(
+            self,
+            n_estimators=100,
+            *,
+            criterion="gini",
+            max_depth=None,
+            min_samples_split=2,
+            min_samples_leaf=1,
+            min_weight_fraction_leaf=0.0,
+            max_features="sqrt",
+            max_leaf_nodes=None,
+            min_impurity_decrease=0.0,
+            bootstrap=True,
+            oob_score=False,
+            n_jobs=None,
+            random_state=None,
+            verbose=0,
+            warm_start=False,
+            class_weight=None,
+            ccp_alpha=0.0,
+            max_samples=None,
+            monotonic_cst=None,
+            block_size=1
+    ):
+        super().__init__(
+            n_estimators=n_estimators,
+            criterion=criterion,
+            max_depth=max_depth,
+            min_samples_split=min_samples_split,
+            min_samples_leaf=min_samples_leaf,
+            min_weight_fraction_leaf=min_weight_fraction_leaf,
+            max_features=max_features,
+            max_leaf_nodes=max_leaf_nodes,
+            min_impurity_decrease=min_impurity_decrease,
+            bootstrap=bootstrap,
+            oob_score=oob_score,
+            n_jobs=n_jobs,
+            random_state=random_state,
+            verbose=verbose,
+            warm_start=warm_start,
+            class_weight=class_weight,
+            ccp_alpha=ccp_alpha,
+            max_samples=max_samples,
+            monotonic_cst=monotonic_cst,
+        )
+        self.block_size = block_size
+
+
     # Función copypasteada desde la clase sklearn.ensemble._forest
     def fit(self, X, y, sample_weight=None):
 
@@ -226,6 +278,7 @@ def _parallel_build_trees_with_blocks(
     class_weight=None,
     n_samples_bootstrap=None,
     missing_values_in_feature_mask=None,
+    block_size=1
 ):
     """
     Private function used to fit a single tree in parallel."""
@@ -241,21 +294,28 @@ def _parallel_build_trees_with_blocks(
 
         # Acá le da pesos a los datos de forma aleatoria
         # Se puede modificar acá así se le da peso a uno y a los N siguientes...
-        indices = _generate_sample_indices(
-            tree.random_state, n_samples, n_samples_bootstrap
-        )
-        sample_counts = np.bincount(indices, minlength=n_samples)
-        curr_sample_weight *= sample_counts
         # TODO: Modificar
+        # indices = _generate_sample_indices(
+        #     tree.random_state, n_samples, n_samples_bootstrap
+        # )
+        #sample_counts = np.bincount(indices, minlength=n_samples)
+        #sample_counts = [peso índice 0, peso índice 1, ..., peso índice N]
+        pivote = np.random.randint(n_samples)
+        sample_counts = [0] * n_samples
+        for i in range(block_size):
+            sample_counts[(pivote + i) % n_samples] = 1
+        curr_sample_weight *= sample_counts
+
+
         #########################################################################
         # No sé que hace esto pero desordena los pesos, quitar si es posible
-        if class_weight == "subsample":
-            with catch_warnings():
-                simplefilter("ignore", DeprecationWarning)
-                curr_sample_weight *= compute_sample_weight("auto", y, indices=indices)
-        elif class_weight == "balanced_subsample":
-            curr_sample_weight *= compute_sample_weight("balanced", y, indices=indices)
         # TODO: Intentar borrarlo
+        # if class_weight == "subsample":
+        #     with catch_warnings():
+        #         simplefilter("ignore", DeprecationWarning)
+        #         curr_sample_weight *= compute_sample_weight("auto", y, indices=indices)
+        # elif class_weight == "balanced_subsample":
+        #     curr_sample_weight *= compute_sample_weight("balanced", y, indices=indices)
         ###########################################################################
         tree._fit(
             X,
